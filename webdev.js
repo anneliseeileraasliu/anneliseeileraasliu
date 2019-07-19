07b8704a9dd030483fc9e6d072cd218ef207047a
anneliseeileraasliu <53100845+anneliseeileraasliu@users.noreply.github.com>
2019-07-19 15:58:00 -0700

    Add files via upload

---
+++ webdev.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+alert("Hello")
+function gotoform() {
+
+//document.getElementById("...")
+
+gotoform()=
+	href="https://docs.google.com/forms/d/e/1FAIpQLSear7QSHUQX6fLQFu7JxJQWtnGinz8p-Lp7gt-8qy5oG0eJVg/viewform"
+}
